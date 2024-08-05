17ad902e0088e43c2cf2e735958677f5d8470de0
BrayaCheat <prayacheat@gmail.com>
2024-08-05 11:32:37 +0700

    Implement search filter

--- app/api/todo/filter/route.ts
+++ app/api/todo/filter/route.ts
@@ -6,32 +6,36 @@ export async function GET(request: Request) {
     const url = new URL(request.url);
     const query = url.searchParams.get("query");
 
-    const { data, error } = await supabase
-      .from("ToDoList")
-      .select("*")
-      .ilike("todo", `%${query}%`);
+    if (query) {
+      const { data, error } = await supabase
+        .from("ToDoList")
+        .select("*")
+        .ilike("todo", `%${query}%`);
 
-    if (error) {
-      return NextResponse.json(
-        {
-          message: "Failed with supabase",
-        },
-        { status: 500 }
-      );
-    }
+      if (error) {
+        return NextResponse.json(
+          {
+            message: "Failed with supabase",
+          },
+          { status: 500 }
+        );
+      }
 
-    if (!data.length) {
-      return NextResponse.json(
-        {
-          message: "Task not found",
-        },
-        { status: 404 }
-      );
+      if (!data.length) {
+        return NextResponse.json(
+          {
+            message: "Task not found",
+          },
+          { status: 404 }
+        );
+      }
+      return NextResponse.json({
+        data: data,
+      });
+    } else {
+      throw new Error("Missing query!");
     }
-    return NextResponse.json({
-      data: data,
-    });
   } catch (error) {
-    throw new Error("Fail to filter task")
+    throw new Error("Fail to filter task");
   }
 }
