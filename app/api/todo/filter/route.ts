340f52fb02b3f8b6007ac1c2b96e72074f4400cb
BrayaCheat <prayacheat@gmail.com>
2024-08-05 12:14:55 +0700

    implement search filter

--- app/api/todo/filter/route.ts
+++ app/api/todo/filter/route.ts
@@ -1,39 +1,46 @@
-// import supabase from "@/app/config/supabaseClient";
-// import { NextResponse } from "next/server";
+import supabase from "@/app/config/supabaseClient";
+import { NextResponse } from "next/server";
 
-// export async function GET(request: Request) {
-//   try {
-//     const url = new URL(request.url);
-//     const query = url.searchParams.get("query") || "";
+export async function GET(request: Request) {
+  try {
+    if (request.method === "GET") {
+      const url = new URL(request.url);
+      const query = url.searchParams.get("query") || "";
 
-    
-//       const { data, error } = await supabase
-//         .from("ToDoList")
-//         .select("*")
-//         .ilike("todo", `%${query}%`);
+      const { data, error } = await supabase
+        .from("ToDoList")
+        .select("*")
+        .ilike("todo", `%${query}%`);
 
-//       if (error) {
-//         return NextResponse.json(
-//           {
-//             message: "Failed with supabase",
-//           },
-//           { status: 500 }
-//         );
-//       }
+      if (error) {
+        return NextResponse.json(
+          {
+            message: "Failed with supabase",
+          },
+          { status: 500 }
+        );
+      }
 
-//       if (!data.length) {
-//         return NextResponse.json(
-//           {
-//             message: "Task not found",
-//           },
-//           { status: 404 }
-//         );
-//       }
-//       return NextResponse.json({
-//         data: data,
-//       });
-    
-//   } catch (error) {
-//     throw new Error("Fail to filter task");
-//   }
-// }
+      if (!data.length) {
+        return NextResponse.json(
+          {
+            message: "Task not found",
+          },
+          { status: 404 }
+        );
+      }
+      return NextResponse.json({
+        data: data,
+      });
+    } else {
+      return NextResponse.json(
+        {
+          message: "Method not allowed",
+        },
+        { status: 405 }
+      );
+    }
+  } catch (error) {
+    throw new Error("Fail to filter task");
+  }
+}
