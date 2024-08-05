3a4866741f33ddd4bbd1947c48af573a032dfc02
BrayaCheat <prayacheat@gmail.com>
2024-08-05 11:36:21 +0700

    Implement search filter

--- app/api/todo/filter/route.ts
+++ app/api/todo/filter/route.ts
@@ -3,38 +3,38 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   try {
-    const url = new URL(request.url);
-    const query = url.searchParams.get("query");
+    // const url = new URL(request.url);
+    // const query = url.searchParams.get("query");
 
-    if (query) {
-      const { data, error } = await supabase
-        .from("ToDoList")
-        .select("*")
-        .ilike("todo", `%${query}%`);
+    // if (query) {
+    //   const { data, error } = await supabase
+    //     .from("ToDoList")
+    //     .select("*")
+    //     .ilike("todo", `%${query}%`);
 
-      if (error) {
-        return NextResponse.json(
-          {
-            message: "Failed with supabase",
-          },
-          { status: 500 }
-        );
-      }
+    //   if (error) {
+    //     return NextResponse.json(
+    //       {
+    //         message: "Failed with supabase",
+    //       },
+    //       { status: 500 }
+    //     );
+    //   }
 
-      if (!data.length) {
-        return NextResponse.json(
-          {
-            message: "Task not found",
-          },
-          { status: 404 }
-        );
-      }
-      return NextResponse.json({
-        data: data,
-      });
-    } else {
-      throw new Error("Missing query!");
-    }
+    //   if (!data.length) {
+    //     return NextResponse.json(
+    //       {
+    //         message: "Task not found",
+    //       },
+    //       { status: 404 }
+    //     );
+    //   }
+    //   return NextResponse.json({
+    //     data: data,
+    //   });
+    // } else {
+    //   throw new Error("Missing query!");
+    // }
   } catch (error) {
     throw new Error("Fail to filter task");
   }
